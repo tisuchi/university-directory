c9467d2ecd1de27a44028579fece63ebb9570451
Thouhedul.Islam <tisuchi@gmail.com>
2026-02-26 16:14:42 +0100

    Add UniversityResource for consistent API responses
    US-018: JSON resource with id, name, short_name, slug, country_code, type, aliases, official_website

---
+++ src/Http/Resources/UniversityResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tisuchi\UniversityDirectory\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UniversityResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'short_name' => $this->short_name,
+            'slug' => $this->slug,
+            'country_code' => $this->country_code,
+            'type' => $this->type?->value,
+            'aliases' => $this->aliases,
+            'official_website' => $this->official_website,
+        ];
+    }
+}
