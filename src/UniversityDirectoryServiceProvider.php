3a4658bece982004e55507811a8604a1e74ae76b
Thouhedul.Islam <tisuchi@gmail.com>
2026-02-26 16:11:22 +0100

    Add package foundation: composer.json, directory structure, service provider
    US-001: composer.json with PSR-4 autoload and Laravel auto-discovery
    US-002: Standard Laravel package directory structure
    US-003: Minimal service provider shell

---
+++ src/UniversityDirectoryServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tisuchi\UniversityDirectory;
+
+use Illuminate\Support\ServiceProvider;
+
+class UniversityDirectoryServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        // TODO: Load migrations (US-027)
+        // TODO: Register commands (US-028)
+        // TODO: Add publishable migrations (US-029)
+    }
+}
