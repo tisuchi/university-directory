0d39a42935aed031ac0c358ef624ca12a1db8958
Thouhedul.Islam <tisuchi@gmail.com>
2026-02-26 16:12:12 +0100

    Add UniversityType enum and ud_universities migration
    US-004: String-backed enum with University, College, Institute, Academy
    US-005: Migration with all columns, indexes, and composite index

---
+++ src/Enums/UniversityType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tisuchi\UniversityDirectory\Enums;
+
+enum UniversityType: string
+{
+    case University = 'university';
+    case College = 'college';
+    case Institute = 'institute';
+    case Academy = 'academy';
+}
